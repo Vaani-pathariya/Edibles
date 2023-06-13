be9579e3513f12e61cc432eec828377470e243f5
Vanshika Jain <106269077+vanshika8750@users.noreply.github.com>
2023-06-13 20:07:38 +0530

    Backend enhanced

--- frontend/src/pages/Post.jsx
+++ frontend/src/pages/Post.jsx
@@ -3,35 +3,50 @@ import axios from "axios"
 import Nav from "../components/Nav.jsx";
 const Post=()=>{
 
-    const [post,setPost]=useState({
-        tagline:"",name:"",ingredients:"",recipe:""
-    })
-    const handleFieldChange=(e)=>{
-        const{tagline,name,ingredients,recipe}=e.target;
-        // setPost({...post,[name]:[value]});
+    const [tagline,setTagline]=useState("");
+    const [name,setName]=useState("");
+    const [ingredients,setIng]=useState("");
+    const [recipe,setRecipe]=useState("");
+    const handleTagline=(e)=>{
+        setTagline(e.target.value);
+    }
+    const handleName=(e)=>{
+        setName(e.target.value);
+    }
+    const handleIng=(e)=>{
+        setIng(e.target.value);
+    }
+    const handleRecipe=(e)=>{
+        setRecipe(e.target.value);
     }
     const createpost=async(e)=>{
-        e.prevent.Default();
-        const res=await axios.post("http://localhost:4000/post",post);
-        setPost({
-            tagline:res.data.tagline,
-            name:res.data.name,
-            ingredients:res.data.ingredients,
-            recipe:res.data.recipe,
-        })
+        e.preventDefault()
+        const body={
+            tagline: tagline,
+            name:name,
+            ingredients:ingredients,
+            recipe:recipe
+        }
+        const sendingPost=async()=>{
+            try{
+                const response=await axios.post("localhost:4000/post",body);
+                console.log(response);
+            }catch(error){
+                console.log(error);
+            }
+        }
     }
-
     return (
         <div>
             <Nav/>
-            <form onSubmit={createpost} className="post-form">
-            <input onChange={handleFieldChange} value={post.tagline} placeholder="Tagline"></input>
-            <input  onChange={handleFieldChange} value={post.name} placeholder="Name of the recipie"></input>
-            <input  onChange={handleFieldChange} value={post.ingredients} placeholder="Ingredients"></input>
-            <input  onChange={handleFieldChange} value={post.recipe} placeholder="Recipie"></input>
+            <form className="post-form">
+            <input onChange={handleTagline}  placeholder="Tagline" type="text"></input>
+            <input type="text" onChange={handleName} placeholder="Name of the recipie"></input>
+            <input type="text" onChange={handleIng} placeholder="Ingredients"></input>
+            <input type="text" onChange={handleRecipe} placeholder="Recipie"></input>
             {/* <input placeholder="video/audio"></input>
             <input placeholder="pic please"></input> */}
-            <button type="submit">Post</button>
+            <button onClick={createpost}>Post</button>
             </form>
         </div>
     )
